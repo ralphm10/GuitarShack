36060961606a0c903e7063c1425f8010581a239f
Ralph Mallett <rmallet6@ford.com>
2021-11-24 11:51:37 +0000

    Test to return start date

--- src/main/java/com/guitarshack/LastMonthRateOfSales.java
+++ src/main/java/com/guitarshack/LastMonthRateOfSales.java
@@ -1,14 +1,23 @@
 package com.guitarshack;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class LastMonthRateOfSales implements RateOfSales {
     private final SalesHistory salesHistory;
+    private final Today today;
 
     public LastMonthRateOfSales(SalesHistory salesHistory, Today today) {
         this.salesHistory = salesHistory;
+        this.today = today;
     }
 
     @Override
     public double perDay(int productId) {
-        return salesHistory.totalSales(productId, null, null) / 30 ;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(today.get());
+        calendar.add(Calendar.DATE,-30);
+        Date startDate = calendar.getTime();
+        return salesHistory.totalSales(productId, startDate, null) / 30 ;
     }
 }
--- src/test/java/com/guitarshack/RateOfSalesTest.java
+++ src/test/java/com/guitarshack/RateOfSalesTest.java
@@ -1,20 +1,45 @@
 package com.guitarshack;
 
+import org.junit.Before;
 import org.junit.Test;
 import java.util.Calendar;
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class RateOfSalesTest {
 
+    private RateOfSales rateOfSales;
+    private Calendar calendar;
+    private SalesHistory salesHistory;
+
     @Test
     public void calculateRateOfSales() {
-        SalesHistory salesHistory = (productId, startDate, endDate) -> 30;
-        Today today = () -> {
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(2021,Calendar.NOVEMBER,10);
-            return calendar.getTime();
-        };
-        RateOfSales rateOfSales = new LastMonthRateOfSales(salesHistory, today);
         assertEquals(1, rateOfSales.perDay(757), 0);
+    }
+
+    @Before
+    public void setUp() {
+        salesHistory = mock(SalesHistory.class);
+        when(salesHistory.totalSales(anyInt(), any(), any())).thenReturn(30);
+        calendar = Calendar.getInstance();
+        calendar.set(2021,Calendar.NOVEMBER,10);
+        Date todaysDate = calendar.getTime();
+        Today today = () -> todaysDate;
+        rateOfSales = new LastMonthRateOfSales(salesHistory, today);
+    }
+
+    @Test
+    public void calculateStartDate() {
+        calendar.set(2021, Calendar.OCTOBER, 11);
+        Date startDate = calendar.getTime();
+        rateOfSales.perDay(757);
+        verify(salesHistory).totalSales(anyInt(), eq(startDate), any());
     }
 }
