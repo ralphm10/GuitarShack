8fb5ffda6de24e2b6dbb9a07e5a1f99ebc7e9e4d
Ralph Mallett <rmallet6@ford.com>
2021-12-15 12:54:56 +0000

    Refactor - remove error stack trace print

--- src/main/java/com/guitarshack/Service.java
+++ src/main/java/com/guitarshack/Service.java
@@ -5,7 +5,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class Service {
+public abstract class Service {
     protected final String baseUrl;
 
     public Service(String baseUrl) {
@@ -17,8 +17,7 @@ public class Service {
         try {
             Response<R> execute = call.execute();
             result = execute.body();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ignored) {
         }
         return result;
     }
