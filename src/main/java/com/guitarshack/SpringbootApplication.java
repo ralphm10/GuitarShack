18cbe07b2915c7fa3c4becac06395c2e6f87de2f
Yasmin Underdown <yunderdo@ford.com>
2022-01-26 11:47:23 +0000

    Sales listener added to sale endpoint

--- src/main/java/com/guitarshack/SpringbootApplication.java
+++ src/main/java/com/guitarshack/SpringbootApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-    @SpringBootApplication
+import java.util.Calendar;
+import java.util.Date;
+
+@SpringBootApplication
     @RestController
     public class SpringbootApplication {
 
@@ -20,6 +23,25 @@ import org.springframework.web.bind.annotation.RestController;
 
         @GetMapping("/sale")
         public String sale(int productId, int quantity)  {
+            Today today = () -> Calendar.getInstance().getTime();
+            Notification notification = text -> {};
+            SalesListener salesListener = new SalesListener(
+                    notification,
+                    new ProductWarehouse(
+                            new RetrofitService("https://6hr1390c1j.execute-api.us-east-2.amazonaws.com")
+                    ),
+                    new LeadTimeBufferStock(
+                            new LastMonthRateOfSales(
+                                    new ProductSalesHistory(
+                                            new SalesWebService(
+                                                    new RetrofitService("https://gjtvhjg8e9.execute-api.us-east-2.amazonaws.com")
+                                            )
+                                    ),
+                                    new DateRangeCalculator(today)
+                            )
+                    )
+            );
+            salesListener.onSale(productId, quantity);
             return quantity + " of " + productId + " sold " ;
         }
 
